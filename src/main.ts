efe99da4acc4deb59bac89fd2f40a762678dbb5f
Max Ward <max.jp.ward@gmail.com>
2024-10-02 13:19:27 -0700

    add pinia for store management

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,13 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import './assets/tailwind.css'
-import router from './router'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import './assets/tailwind.css';
+import router from './router';
+import { createPinia } from 'pinia';
 
-createApp(App).use(router).mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(router);
+app.use(pinia);
+app.mount('#app');
